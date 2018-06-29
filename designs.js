4736877efdc3107359fb6303d0d6a4c4d5d18c89
kingfadh <40701181+kingfadh@users.noreply.github.com>
2018-06-29 19:54:35 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#sizePicker").submit(function makeGrid(){
+        4('table tr').remove();
+        var rows = $("#input_height").val();
+        var cols = $("#input_weight").val();
+            for(var i = 1; i <= rows; i++) {
+                $("table").append("<tr></tr>");
+                for(var j = 1; j <= cols; j++){
+                    $("tr:last").append("<td></td>");
+                    $("td").attr("class", "cells");
+                }
+
+            }
+         grid.preventDefault();   
+
+         $(".cells").click(function(event){
+            var paint = $("#colorPicker").val();
+            $(event.target).css("background-color", paint)
+        })
+        
+    })
+
+
+});
+
+
+
+// Your code goes here! 
+
+
